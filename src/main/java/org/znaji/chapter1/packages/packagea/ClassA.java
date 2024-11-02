03b4c8ca3f95dddf6f4e4daac1472ab2558f8803
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-11-02 14:57:30 +0100

    #Chapter1: compiling with cmdLine - java -d <destination> <fullPackageName>.ClassA/B.java - java -cp <classpath = destination> <fullPackageName>.ClassB

---
+++ src/main/java/org/znaji/chapter1/packages/packagea/ClassA.java
@@ -0,0 +1,4 @@
+package org.znaji.chapter1.packages.packagea;
+
+public class ClassA {
+}
---
+++ src/main/java/org/znaji/chapter1/packages/packageb/ClassB.java
@@ -0,0 +1,10 @@
+package org.znaji.chapter1.packages.packageb;
+
+import org.znaji.chapter1.packages.packagea.ClassA;
+public class ClassB {
+
+    public static void main(String[] args) {
+        ClassA a;
+        System.out.println("Hello world!");
+    }
+}
