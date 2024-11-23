a5f6ceff1f8585146552dbd78e644c9b4b2751c6
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-11-23 15:03:33 +0100

    #Chapter4 Strings Methods: - length() -> return length of the string. - chatAt(index) -> return chat ant index(int) - indexOf(int|String, int) -> find index of char/string (1st occurence) optionaly we ca add a starting index to look from.

--- src/main/java/org/znaji/chapter4/strings/StringMethods.java
+++ src/main/java/org/znaji/chapter4/strings/StringMethods.java
@@ -4,10 +4,24 @@ public class StringMethods {
 
     public static void main(String[] args) {
         final String s = "Hello, World!";
-        System.out.println("The length of the string is: " + stringLength(s));
+        //char at index
+        //System.out.println(charAt(s, 0));
+        //System.out.println(charAt(s, 100));//StringIndexOutOfBoundsException
+
+        //index of
+        System.out.println(s.indexOf('o'));
+        System.out.println(s.indexOf('o', 5));
+        System.out.println(s.indexOf("World"));
+        System.out.println(s.indexOf("World", 7));
+        System.out.println(s.indexOf("World", 8));//-1
+        System.out.println(s.indexOf("\u0048"));//0
     }
 
     public static int stringLength(String s) {
         return s.length();
+    }
+
+    public static char charAt(String s, int index) {
+        return s.charAt(index);
     }
 }
