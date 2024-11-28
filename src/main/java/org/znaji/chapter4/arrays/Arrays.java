436931ab99c03b8e4e8a06634502d3a5da0ef40c
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-11-28 19:59:26 +0100

    #Chapter4 Arrays: java.utils.Arrays provide a list of useful methods to handle arrays: -Arrays.equal() => content equality not just ref -Arrays.sort() => sort an array -Arrays.binarySearch(arr, element) => search for elemnt in array and return it's index(array need to be sorted 1st) -Arrays.compare(arr1, arr2): +(elemnts of arr1 > arr2); -(for the inverse); 0(arrays equal) -Arrays.mismatch(arr1, arr2) => -1(arrays are equal); else index of the 1st mismatch.

--- src/main/java/org/znaji/chapter4/arrays/Arrays.java
+++ src/main/java/org/znaji/chapter4/arrays/Arrays.java
@@ -18,7 +18,28 @@ public class Arrays {
         int[] nums2 = {10, 20, 30};
         System.out.println(nums.equals(nums2));//equals not overridden in arrays so it compares the reference
 
-        //nums2 = {10, 20, 30};//compilation error => only allowed during declaration
-        nums2 = new int[]{10, 20, 30};//allowed => {} is used to initialize the array
+        //nums2 = {10, 20, 30};//compilation error => can't infer the type of the array
+        nums2 = new int[]{10, 20, 30};//allowed because the type is already known
+
+        //Array methods
+        System.out.println(nums.length);
+        int nums3[] = {10, -5, 80, 0};
+        java.util.Arrays.sort(nums3);
+        System.out.println(java.util.Arrays.toString(nums3));
+        System.out.println(java.util.Arrays.binarySearch(nums3, 80));
+        System.out.println(java.util.Arrays.binarySearch(nums3, 100));//-5(theoretical position)
+
+        System.out.println(java.util.Arrays.equals(nums, nums2));//compare the content of the arrays not the reference
+
+        System.out.println(java.util.Arrays.compare(new int[]{1, 2, 3}, new int[]{1, 2, 3}));//0 => equal
+        System.out.println(java.util.Arrays.compare(new int[]{1, 2, 3, 4}, new int[]{1, 2, 3}));//1 => first array is greater
+        System.out.println(java.util.Arrays.compare(new int[]{1, 2, 3}, new int[]{1, 2, 3, 4}));//-1 => second array is greater
+        System.out.println(java.util.Arrays.compare(new String[]{"a", "b", "c"}, new String[]{"a", "b", "c"}));//0 => equal
+        System.out.println(java.util.Arrays.compare(new String[]{"a", "b", "c"}, new String[]{"x", "y", "z"}));//-23 => first array is greater
+        System.out.println(java.util.Arrays.compare(new String[]{"x", "y", "z"}, new String[]{"a", "b", "c"}));//23 => second array is greater
+
+        System.out.println(java.util.Arrays.mismatch(new int[]{1, 2, 3}, new int[]{1, 2, 3}));//-1 => no mismatch
+        System.out.println(java.util.Arrays.mismatch(new int[]{1, 2, 3, 4}, new int[]{1, 2, 3}));//3 => mismatch at index 3
     }
+
 }
